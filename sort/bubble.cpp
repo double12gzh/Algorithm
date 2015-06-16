a17d029facc636c65f47d4870b626a535e6f5187
CV0033106 <double12gzh@icloud.com>
2015-06-16 23:29:36 +0800

    write swap and bubble func

--- sort/bubble.cpp
+++ sort/bubble.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
-int bubble()
+template<typename T>
+void Swap(T &a, T &b)
 {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+int bubble(int array[], int left, int right)
+{
+	int i = left;
+	int j = right;
+	bool b_Flag = FALSE;
+
+	for (; i<n-1; i++)
+	{
+		for (; j>i; j--)
+		{
+			if (array[j] < array[j-1])
+			{
+				Swap(array[j], array[j-1]);
+				b_Flag = TRUE;
+			}
+		}
+		
+		if (b_Flag == FALSE)
+			break;
+	}
+	
 	return 0;  
 }
